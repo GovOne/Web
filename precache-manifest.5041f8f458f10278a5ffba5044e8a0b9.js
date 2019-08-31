fc0307b38b8f1e3e25156923dab9a92a545e4e7d
Q <questappsza@gmail.com>
2019-08-31 16:39:35 +0200

    Update

---
+++ precache-manifest.5041f8f458f10278a5ffba5044e8a0b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b048267c729c1469ff24cec5708f2ebb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7a6b6b3847f35b6dd96",
+    "url": "/static/css/main.b13fb86d.chunk.css"
+  },
+  {
+    "revision": "cc3b84f4c9198cbe3ee9",
+    "url": "/static/js/2.a7a42186.chunk.js"
+  },
+  {
+    "revision": "f7a6b6b3847f35b6dd96",
+    "url": "/static/js/main.b4301c0c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
